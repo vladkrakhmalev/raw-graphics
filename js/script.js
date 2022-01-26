9a23ab7ae69197083276c383212fb9dd6753be7e
Vlad Krakhmalev <93250777+vladkrakhmalev@users.noreply.github.com>
2022-01-26 20:22:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector('body').classList.add('_webp');
+	} else {
+		document.querySelector('body').classList.add('_no-webp');
+	}
+});
+
+
+$(document).ready(function () {
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function () {
+				if ($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+});
